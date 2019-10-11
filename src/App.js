f15485576c5a540ad988f94a526ddf82a83a2865
Joshua <joshchurch1996@gmail.com>
2019-10-10 22:21:16 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -5,11 +5,24 @@ import './App.css';
 function importAll(r) {
   return r.keys().map(r);
 }
-const SQUARES = importAll(require.context('./assets/board_squares', false, /\.(png|jpe?g|svg)$/));
-const PIECES = importAll(require.context('./assets/svg_no_shadow', false, /\.(png|jpe?g|svg)$/));
+// const SQUARES = importAll(require.context('./assets/board_squares', false, /\.(png|jpe?g|svg)$/));
+
+let pieces     = importAll(require.context('./assets/svg_no_shadow', false, /\.(png|jpe?g|svg)$/));
+const B_BISHOP = pieces[0]
+const B_KING   = pieces[1]
+const B_KNIGHT = pieces[2]
+const B_PAWN   = pieces[3]
+const B_QUEEN  = pieces[4]
+const B_ROOK   = pieces[5]
+const W_BISHOP = pieces[6]
+const W_KING   = pieces[7]
+const W_KNIGHT = pieces[8]
+const W_PAWN   = pieces[9]
+const W_QUEEN  = pieces[10]
+const W_ROOK   = pieces[11]
+
 
 function App() {
-  //divide chess-board into ranks and files? using css pixels to display chessboard may be keeping them from touching
   return (
     <div className="App">
       <div className="chess-board">
@@ -19,22 +32,32 @@ function App() {
   );
 }
 
+//make this the constructor of a new class
+function init_board() {
+  let board_array = new Array(8);
+  for (let i=0; i<8; i++) {
+    board_array[i] = new Array(8);
+  }
+  return board_array;
+}
+
 //make this a class function
 function create_board() {
-  let bd = SQUARES[0];
-  let bl = SQUARES[1];
-  // let gd = SQUARES[2];
-  // let gl = SQUARES[3];
+  // const brown_dark  = SQUARES[0];
+  // const brown_light = SQUARES[1];
+  // const gray_dark   = SQUARES[2];
+  // const gray_light  = SQUARES[3];
+  // const alternate = [bd,bl];
+
   let board = init_board();
   // let piece_position
-  let alternate = [bd,bl];
   let flip = 'square white-square';
   let count = -1
 
   for (let file=0; file<board[0].length; file++) {
     for (let rank=0; rank<board[0].length; rank++) {
       count ++;
-      let piece = get_file_piece(rank,file);
+      let piece = get_piece(rank,file);
 
       board[file][rank]=(
       <div class={flip}>
@@ -47,60 +70,51 @@ function create_board() {
   return board
 }
 
-//make this the constructor of a new class
-function init_board() {
-  let board_array = new Array(8);
-  for (let i=0; i<8; i++) {
-    board_array[i] = new Array(8);
-  }
-  return board_array;
-}
-
-function get_file_piece(rank, file) {
-  if (file === 0) {
-    switch (rank) {
-      case (rank===0): {
-        return PIECES[5];
-      } case (rank===1):  {
-        return PIECES[2];
-      } case (rank===2): {
-        return PIECES[0];
-      } case (rank===3): {
-        return PIECES[4];
-      } case (rank===4): {
-        return PIECES[1];
-      } case (rank===5): {
-        return PIECES[0];
-      } case (rank===6): {
-        return PIECES[2];
-      } case (rank===7): {
-        return PIECES[5];
+function get_piece(file, rank) {
+  if (rank === 0) {
+    switch (file) {
+      case (file===0): {
+        return B_ROOK;
+      } case (file===1):  {
+        return B_KNIGHT;
+      } case (file===2): {
+        return B_BISHOP;
+      } case (file===3): {
+        return B_QUEEN;
+      } case (file===4): {
+        return B_KING;
+      } case (file===5): {
+        return B_BISHOP;
+      } case (file===6): {
+        return B_KNIGHT;
+      } case (file===7): {
+        return B_ROOK;
       }
     }
-  } else if (file === 7) {
-    switch (rank) {
-      case (rank===0): {
-        return PIECES[11];
-      } case (rank===1):  {
-        return PIECES[8];
-      } case (rank===2): {
-        return PIECES[6];
-      } case (rank===3): {
-        return PIECES[10];
-      } case (rank===4): {
-        return PIECES[7];
-      } case (rank===5): {
-        return PIECES[6];
-      } case (rank===6): {
-        return PIECES[8];
-      } case (rank===7): {
-        return PIECES[11];
+  } else if (rank === 7) {
+    switch (file) {
+      case (file===0): {
+        return W_ROOK;
+      } case (file===1):  {
+        return W_KNIGHT;
+      } case (file===2): {
+        return W_BISHOP;
+      } case (file===3): {
+        return W_QUEEN;
+      } case (file===4): {
+        return W_KING;
+      } case (file===5): {
+        return W_BISHOP;
+      } case (file===6): {
+        return W_KNIGHT;
+      } case (file===7): {
+        return W_ROOK;
       }
     }
-  } else if (file === 1) {
-    return PIECES[3]
-  } else if (file === 6) {
-    return PIECES[9]
+  } else if (rank === 1) {
+    return B_PAWN
+  } else if (rank === 6) {
+    return W_PAWN
   }
 }
 
