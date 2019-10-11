a48f47e134d393dc5ee0b28905fdef1c4b571b64
Joshua <joshchurch1996@gmail.com>
2019-10-10 22:54:06 -0400

    class working

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
 import './App.css';
-import './board.js'
-
-function importAll(r) {
-  return r.keys().map(r);
-}
-// const SQUARES = importAll(require.context('./assets/board_squares', false, /\.(png|jpe?g|svg)$/));       //probs not needed
-
-let pieces     = importAll(require.context('./assets/svg_no_shadow', false, /\.(png|jpe?g|svg)$/));
-const B_BISHOP = pieces[0]
-const B_KING   = pieces[1]
-const B_KNIGHT = pieces[2]
-const B_PAWN   = pieces[3]
-const B_QUEEN  = pieces[4]
-const B_ROOK   = pieces[5]
-const W_BISHOP = pieces[6]
-const W_KING   = pieces[7]
-const W_KNIGHT = pieces[8]
-const W_PAWN   = pieces[9]
-const W_QUEEN  = pieces[10]
-const W_ROOK   = pieces[11]
-
+import Board from './board.js';
 
 function App() {
   return (
     <div className="App">
       <div className="chess-board">
-        {board = new Board()}
+        {new Board().board}
       </div>
     </div>
   );
--- src/board.js
+++ src/board.js
@@ -1,24 +1,45 @@
-import { tsConstructorType } from "@babel/types";
+import React from 'react';
+function importAll(r) {
+  return r.keys().map(r);
+}
 
-class Board {
+let pieces     = importAll(require.context('./assets/svg_no_shadow', false, /\.(png|jpe?g|svg)$/));
+const B_BISHOP = pieces[0]
+const B_KING   = pieces[1]
+const B_KNIGHT = pieces[2]
+const B_PAWN   = pieces[3]
+const B_QUEEN  = pieces[4]
+const B_ROOK   = pieces[5]
+const W_BISHOP = pieces[6]
+const W_KING   = pieces[7]
+const W_KNIGHT = pieces[8]
+const W_PAWN   = pieces[9]
+const W_QUEEN  = pieces[10]
+const W_ROOK   = pieces[11]
+
+export default class Board {
   constructor() {
     this.board_array = new Array(8);
     for (let i=0; i<8; i++) {
       this.board_array[i] = new Array(8);
     }
-    create_board()
+    this.create_board()
+  }
+
+  get board() {
+    return this.board_array;
   }
       
   create_board() {
     let flip = 'square white-square';
     let count = -1
     
-    for (let file=0; file<this.board[0].length; file++) {
-      for (let rank=0; rank<this.board[0].length; rank++) {
+    for (let file=0; file<this.board_array[0].length; file++) {
+      for (let rank=0; rank<this.board_array[0].length; rank++) {
       count ++;
-      let piece = _get_piece(rank,file);
+      let piece = this.get_piece(rank,file);
   
-      this.board[file][rank]=(
+      this.board_array[file][rank]=(
         <div class={flip}>
           <img src={piece} className="chess-piece" alt={piece} key={(count)}/>
         </div>
@@ -29,7 +50,7 @@ class Board {
     }
   }
       
-  _get_piece(file, rank) {
+  get_piece(file, rank) {
     if (rank === 0) {
       switch (file) {
         case (file===0): {
